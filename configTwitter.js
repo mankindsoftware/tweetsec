313d98871bbe8353c345ee7a1a23516627fe89eb
Mark Hall <mankindsoftware@gmail.com>
2015-04-10 21:12:38 -0700

    config file example

---
+++ configTwitter.js
@@ -0,0 +1,8 @@
+// Fill in and place in the config folder.
+module.exports = {
+	owner: 'MarkHall2000',
+    consumer_key: 'fghftttt', 
+    consumer_secret: 'fhggfh', 
+  	access_token: 'fhgfhgh-dfgdfg', 
+  	access_token_secret: 'drdgfgccccc'
+};
